44270aa3f7be30623449e53f9b552f192625363f
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-03-25 21:27:04 +0100

    commit

--- src/main/java/com/onlyServlet/impl/tryServlet_impl.java
+++ src/main/java/com/onlyServlet/impl/tryServlet_impl.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 @WebServlet("/tryServlet_impl")
 public class tryServlet_impl extends HttpServlet{
 
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         response.getWriter().write("<h1>Hello, Servlet!</h1>");
