fd5597325d8ad42dc02fe2e5f7b8234d0801b71b
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-03-26 21:02:20 +0100

    commit

--- src/main/java/com/onlyServlet/impl/tryServlet_impl.java
+++ src/main/java/com/onlyServlet/impl/tryServlet_impl.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/tryServlet_impl")
+@WebServlet("/test")
 public class tryServlet_impl extends HttpServlet{
 
     @Override
